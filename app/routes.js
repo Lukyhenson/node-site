d76887d29670095ec4beb1e09ac38394bee8df54
DESKTOP-08500Q6\lukyh <lukyhenson@live.com>
2016-11-15 04:07:48 -0200

    changes in routes about

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,14 @@ router.get('/', function(req, res){
 
 // rout for our about page
 router.get('/about', function(req, res){
-  res.render('pages/about');
+  var users = [
+    { name: 'Tauan', office: 'WEB-Developer',  email: 'Tauan@node-website.io', avatar: 'https://images.pexels.com/photos/119705/pexels-photo-119705.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
+    { name: 'James', office: 'Design',  email: 'James@node-website.io', avatar: 'https://images.pexels.com/photos/119705/pexels-photo-119705.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
+    { name: 'John', office: 'UI Design',  email: 'John@node-website.io', avatar: 'https://images.pexels.com/photos/119705/pexels-photo-119705.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
+    { name: 'Holly', office: 'Data-Base',  email: 'Holly@node-website.io', avatar: 'https://images.pexels.com/photos/119705/pexels-photo-119705.jpeg?w=940&h=650&auto=compress&cs=tinysrgb' },
+  ];
+
+  res.render('pages/about', { users: users });
 });
 
 router.get('/contact', function(req, res){
